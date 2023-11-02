e25b9b49164f72f9df5c02edf8a3d1c5014fd336
Dhruvpathak25 <dapathak.dp@gmail.com>
2023-11-03 00:30:10 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const typeDefs = require("./schema");
+const resolvers = require("./resolvers");
+
+const app = express();
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+async function startServer() {
+  await server.start();
+  server.applyMiddleware({ app });
+}
+
+startServer().then(() => {
+  app.listen({ port: 3001 }, () =>
+    console.log("Server listening on port", 3001)
+  );
+});
