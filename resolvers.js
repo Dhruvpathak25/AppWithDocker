5175d4e2774556ce2dd2d426d96088f92299ee0d
Dhruvpathak25 <dapathak.dp@gmail.com>
2023-11-03 02:58:00 +0530

    docker update

--- resolvers.js
+++ resolvers.js
@@ -2,7 +2,9 @@ const resolvers = {
   Query: {
     hello: () => {
       // to break the backend
+      console.log("hello docker")
       let sum = sum + inputNumber;
+      
       return "Hello world!";
       
     },
